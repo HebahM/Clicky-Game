caa6858d5d7738969986f48f72c1f32653238eec
HebahM <56455544+HebahM@users.noreply.github.com>
2020-05-01 16:53:02 -0500

    Updates

---
+++ precache-manifest.23fb851b4928bbabcc756f55dde753f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7433d15dea7c7449643fb4642212c3b4",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "c564a29edd50d3c5bd0d",
+    "url": "/Clicky-Game/static/css/main.82193629.chunk.css"
+  },
+  {
+    "revision": "1b46b53cf310619c2461",
+    "url": "/Clicky-Game/static/js/2.fe233ed1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clicky-Game/static/js/2.fe233ed1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c564a29edd50d3c5bd0d",
+    "url": "/Clicky-Game/static/js/main.5a5f5714.chunk.js"
+  },
+  {
+    "revision": "68a5cdd7631e662c82ff",
+    "url": "/Clicky-Game/static/js/runtime-main.28e256fc.js"
+  }
+]);
